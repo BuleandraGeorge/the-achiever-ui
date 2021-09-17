bf0cbd7867ac51a5ca698fd1bbd673a62fc372bb
BuleandraGeorge <57807805+BuleandraGeorge@users.noreply.github.com>
2021-09-17 11:43:04 +0100

    C:\Users\George\Desktop\the-achiever-ui

---
+++ src/Account/SignUp.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export default class SignUp extends from React.Component
+{
+    render()
+    {
+        return(
+            <div className="container">
+                <h1>SignUp to benefit the latest features</h1>
+                <h2>You can do it by using you Google account too.</h2>
+                <div className="container d-flex justify-content-center"
+                <div>
+                    <label for="email">Email</label>
+                    <input type="email" id="email" />
+                </div>
+                <div>
+                    <label for="pass">Password</label>
+                    <input type="password" id="pass" />
+                </div>
+                <div>
+                    <label for="confirm-pass">Confirm Password</label>
+                    <input type="password" id="confirm-pass" />
+                </div>
+            </div>
+        )
+    }
+}
